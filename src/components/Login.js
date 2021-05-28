b53696ce243b57adcaed8bbcf59440292d32e6f0
Kate <kabaron91@gmail.com>
2021-05-28 14:05:23 -0500

    Update 2

---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Login = function () {
+    return (
+        <div id="login">
+            <form>
+                <label for="loginUsername">Username</label>
+                <input type="text" id="loginUsername" name="loginUsername"/><br/>
+                <label for="loginPassword">Password</label>
+                <input type="password" id="loginPassword" name="loginPassword"/><br/>
+                <input type="Submit"/> 
+            </form>
+        </div>
+    )
+}
+
+export default Login 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
+import axios from 'axios' 
+
+import Login from './components/Login'
+
+const BASE_URL = 'https://strangers-things.herokuapp.com/api/2104-UIC-RM-WEB-FT'
+
+
+
+
+
+
+
+
+// ReactDOM.render(
+//     <Login />,  
+//     document.getElementById("app")
+// )
+
